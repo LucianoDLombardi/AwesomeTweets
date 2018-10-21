4175cbf6d2c293c47dc7da7d5a0cc25bad794fed
Luciano Lombardi <ccianocian@gmail.com>
2018-10-21 09:23:41 -0700

    Restructured folder layout in the solution. added Get Tweets view

---
+++ AwesomeTweets/Controllers/GetTweetsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Data;
+
+namespace AwesomeTweets.Controllers
+{
+    public class GetTweetsController : Controller
+    {
+        private readonly TweetContext _dbContext;
+
+        //pass in the database context using dependency injection
+
+        public GetTweetsController(TweetContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Here we pass the downloaded tweets to GetTweets view
+        public IActionResult GetTweets()
+        {
+            return View(_dbContext.AllTweets);
+        }
+
+    }
+}
